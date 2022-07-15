ad02ac76c8e98766969859cfd80ced0bb9f48b36
Alessio Lucciola <alessiolucciola98@hotmail.it>
2022-07-15 17:35:11 +0200

    Products section started

--- frontend/src/constants/images.js
+++ frontend/src/constants/images.js
@@ -4,10 +4,27 @@ import slide1 from '../assets/slide1.jpg';
 import slide2 from '../assets/slide2.jpg';
 import slide3 from '../assets/slide3.jpg';
 
+import animalilogo from '../assets/animali/animali-logo.jpg';
+import cancellilogo from '../assets/cancelli/cancelli-logo.jpg';
+import filagnelogo from '../assets/filagne/filagne-logo.jpg';
+import legnalogo from '../assets/legna/legna-logo.jpg';
+import recinzionilogo from '../assets/recinzioni/recinzioni-logo.jpg';
+import staccionatelogo from '../assets/staccionate/staccionate-logo.jpg';
+import viminatelogo from '../assets/viminate/viminate-logo.jpg';
+import vivaiologo from '../assets/vivaio/vivaio-logo.jpg';
+
 export default {
     logo,
     arrowup,
     slide1,
     slide2,
-    slide3
+    slide3,
+    animalilogo,
+    cancellilogo,
+    filagnelogo,
+    legnalogo,
+    recinzionilogo,
+    staccionatelogo,
+    viminatelogo,
+    vivaiologo
 };
