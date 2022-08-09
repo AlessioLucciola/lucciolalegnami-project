81cdb090c379bbe7f3c43c0ea9f41d4a8253a302
Alessio Lucciola <alessiolucciola98@hotmail.it>
2022-08-09 17:47:31 +0200

    Sitemap generator added

---
+++ src/router.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Switch, Route } from 'react-router';
+
+export default (
+    <Route>
+        <Route path='/prodotti' />
+        <Route path='/prodotti/recinzioni' />
+        <Route path='/prodotti/staccionate' />
+        <Route path='/prodotti/filagne' />
+        <Route path='/prodotti/cancelli' />
+        <Route path='/prodotti/furcine' />
+        <Route path='/prodotti/sostegnopiante' />
+        <Route path='/prodotti/viminate' />
+        <Route path='/prodotti/legnadaardere' />
+        <Route path='/legnoutilizzato' />
+        <Route path='/contatti' />
+        <Route path='/preventivo' />
+    </Route>
+);
